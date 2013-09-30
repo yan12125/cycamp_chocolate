1c8228299ab0e2eca0f02de38a918d7d1cc625da
yan12125 <yan12125@gmail.com>
2013-09-30 21:25:00 +0800

    Add some functions to query.php
    Now order primarily by date

--- query.php
+++ query.php
@@ -10,43 +10,51 @@ header_auth(get_param('username2'), get_param('password2'));
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>雲嘉會巧克力傳情 - 人名查詢</title>
-        <script type="text/javascript" src="/HTML/library/jquery.js"></script>
-        <script>
-            $(document).on('ready', function(e){
-                var run = function(){
-                    $.post('query_core.php', {s: $('#name').val()}, function(data, status, xhr){
-                        if(data.data.length>0)
-                        {
-                            $('#results').html('數量：'+data.count+'<br>總價：$'+data.total+'<br>');
-                            for(var i=0;i<data.data.length;i++)
-                            {
-                                $('#results').append(data.data[i].name+" "+data.data[i].tel+" "+data.data[i].parsed_id+"<br>\n");
-                            }
-                        }
-                        else
-                        {
-                            $('#results').html('無資料');
-                        }
-                    }, 'json');
-                };
-                $('#name').on('keydown', function(e){
-                    if(e.keyCode === 13)
-                    {
-                        run();
-                    }
-                });
-                $('#run').on('click', function(e){
-                    run();
-                });
-            });
-        </script>
-    </head>
-    <body>
-        姓名：<input type="text" id="name">
-        <input type="button" value="查詢" id="run">
-        <div id="results"></div>
-    </body>
+<head>
+<meta charset="UTF-8">
+<title>雲嘉會巧克力傳情 - 人名查詢</title>
+<script type="text/javascript" src="/HTML/library/jquery.js"></script>
+<script>
+$(document).on('ready', function(e){
+    var run = function(){
+        $.post('query_core.php', {s: $('#name').val()}, function(data, status, xhr){
+            if(data.data.length == 0)
+            {
+                $('#results').html('無資料');
+                return;
+            }
+            $('#results').html('數量：'+data.count+'<br>總價：$'+data.total+'<br>');
+            $('#results').append('<table border="1"><tr><td></td><td>姓名</td><td>電話</td><td>駐點</td><td>網路訂單編號</td><td>日期</td></tr></table>');
+            for(var i=0;i<data.data.length;i++)
+            {
+                $('#results table').append(
+                    '<tr>'+
+                        '<td>'+(i+1)+'</td>'+
+                        '<td>'+data.data[i].name+'</td>'+
+                        '<td>'+data.data[i].tel+'</td>'+
+                        '<td>'+data.data[i].stand+'</td>'+
+                        '<td>'+data.data[i].id+'</td>'+
+                        '<td>'+data.data[i].date+'</td>'+
+                    '</tr>'
+                );
+            }
+        }, 'json');
+    };
+    $('#name').on('keydown', function(e){
+        if(e.keyCode === 13)
+        {
+            run();
+        }
+    });
+    $('#run').on('click', function(e){
+        run();
+    });
+});
+</script>
+</head>
+<body>
+    傳情人姓名：<input type="text" id="name">
+    <input type="button" value="查詢" id="run">
+    <div id="results"></div>
+</body>
 </html>
--- query_core.php
+++ query_core.php
@@ -15,7 +15,7 @@ function utf8_to_escaped($s)
 if(isset($_POST['s']))
 {
     $pdo = start_db();
-    $stmt = $pdo->prepare('SELECT orderer,receiver,stand,company,ID,products FROM chocolate WHERE orderer REGEXP ? AND debug="N"');
+    $stmt = $pdo->prepare('SELECT orderer,receiver,stand,company,ID,products,date FROM chocolate WHERE orderer REGEXP ? AND debug="N" ORDER BY date DESC,stand,ID');
     // http://lists.mysql.com/mysql/206935
     $stmt->execute(array('\\{"name":"[^"]*'.str_replace("\\", "\\\\", utf8_to_escaped($_POST['s']))));
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -28,10 +28,13 @@ if(isset($_POST['s']))
     for($i=0;$i<$output['count'];$i++)
     {
         $item = $results[$i];
-        $data = json_decode($item['orderer'], true);
+        $data2 = json_decode($item['orderer'], true);
         $campus = campus($item['orderer'], $item['receiver']);
-        $ID = $item['stand'].$item['company'].$campus.sprintf("%05d", $item['ID']);
-        $data['parsed_id'] = $ID;
+        $data['name'] = $data2['name'];
+        $data['tel'] = $data2['tel'];
+        $data['stand'] = $_data['stands'][$item['stand']]['name'];
+        $data['id'] = sprintf("%04d", $item['ID']);
+        $data['date'] = $item['date'];
         $output['data'][] = $data;
         $output['total'] += price(json_decode($item['products'], true), $item['company'])+fee($item['company'], $campus);
     }
